eb1c6f9c0c5a2c52a93866da8917f9a395378f00
Yonadhan <yonadhanmm0@gmail.com>
2026-01-06 02:33:35 +0530

    added seed

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import 'dotenv/config' // 👈 Load env vars BEFORE everything else
 import app from '@/app.js'
 import { connectDB } from '@/config/db.js'
+import { seedAdmin } from './services/admin.seed'
 
 const PORT = process.env.PORT || 5000
 
 const startServer = async () => {
   await connectDB()
-  //await seedAdmin()
+  await seedAdmin()
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`)
   })
